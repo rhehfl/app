1d2c88ecdbf6d75e386853c3d4fffe1c69d41f77
GuDoYoon <rhehfl418q@gmail.com>
2026-01-26 19:14:54 +0900

    feat: 네이티브윈드 및 Reusables추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
+import '../global.css';
+import { PortalHost } from '@rn-primitives/portal';
 import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <PortalHost />
     </Stack>
   );
 }
